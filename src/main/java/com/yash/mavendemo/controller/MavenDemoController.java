33d7170fda477f6322543e588c7bdee97e5302ec
harmeet.saluja <harmeet.saluja@YI1007116DT.Yash.com>
2018-04-24 14:46:25 +0530

    index page added to take details

--- src/main/java/com/yash/mavendemo/controller/MavenDemoController.java
+++ src/main/java/com/yash/mavendemo/controller/MavenDemoController.java
@@ -3,7 +3,7 @@ package com.yash.mavendemo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +24,7 @@ public class MavenDemoController {
 	}
 
 	@RequestMapping(value = "/saveUser", method = RequestMethod.POST)
-	public String saveUser(@RequestBody User user) {
+	public String saveUser(@ModelAttribute User user) {
 		boolean result = userService.saveUser(user);
 		if (result)
 			return "User saved successfully";
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<html>
+<body>
+	<form action="./demo/saveUser" method="post">
+		Name:&nbsp;<input type="text" name="name" /><br />
+		<br /> Email:&nbsp;<input type="email" name="email" /><br />
+		<br /> <input type="submit" value="Submit" />
+	</form>
+</body>
+</html>
